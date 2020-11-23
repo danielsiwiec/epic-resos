368e41bd42631120be4c3e43a60258ffc95466a9
Dan Siwiec <daniel.siwiec@gmail.com>
2020-11-23 13:22:00 -0800

    Improve styling

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,36 +1,54 @@
 import React from 'react'
 import moment from 'moment'
-import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import styles from '../styles/Home.module.css'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default class Cal extends React.Component {
+const resos = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('resos')) : []
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      resos: []
-    }
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}))
 
-  componentDidMount() {
-    this.setState({ resos: JSON.parse(localStorage.getItem('resos')) })
-  }
+export default function Cal() {
+  const localizer = momentLocalizer(moment)
+  const classes = useStyles()
 
-  render() {
-    const localizer = momentLocalizer(moment)
-
-    return (
-      <Container maxWidth="md">
-          <Calendar
-            localizer={localizer}
-            events={this.state.resos}
-            startAccessor="start"
-            endAccessor="end"
-            style={{ height: 500 }}
-          />
-      </Container>
-    )
-  }
-
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>
+            Total resos: <b>{resos.length}</b>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <Calendar
+              localizer={localizer}
+              events={resos}
+              startAccessor="start"
+              endAccessor="end"
+              style={{ height: 500 }}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,44 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Login from '../components/Login'
 
+import Backdrop from '@material-ui/core/Backdrop'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}))
 
-function Home() {
+export default function Home() {
 
   const [loading, setLoading] = useState(false)
+  const classes = useStyles()
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Epic Reso Manager</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {loading && <CircularProgress />}
-      {!loading && <Login setLoading={setLoading} />}
+      <div className={classes.root}>
+        {loading && <Backdrop open={loading}><CircularProgress /></Backdrop>}
+        {!loading && <Login setLoading={setLoading} />}
+      </div>
     </div>
   )
 
 }
-
-export default Home
