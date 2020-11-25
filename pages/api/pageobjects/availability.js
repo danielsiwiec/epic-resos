46b90b3cb46bd8372de771d6177bf3f1d0fa4ecc
Dan Siwiec <daniel.siwiec@gmail.com>
2020-11-24 18:48:38 -0800

    Return Heavenly availability

--- pages/api/pageobjects/availability.js
+++ pages/api/pageobjects/availability.js
@@ -4,52 +4,24 @@ export default class Availability {
   constructor(page) {
     this.page = page
     this.url = 'https://www.epicpass.com/plan-your-trip/lift-access/reservations.aspx?reservation=true'
-    this.availabilityUrl = `https://www.epicpass.com/api/LiftAccessApi/GetCapacityControlReservationInventory?resortCode=6&startDate=11%2F1%2F2020&endDate=11%2F30%2F2020&_=${moment().valueOf()}`
+    this.availabilityUrl = () => `https://www.epicpass.com/api/LiftAccessApi/GetCapacityControlReservationInventory?resortCode=6&startDate=11%2F1%2F2020&endDate=11%2F30%2F2020&_=${moment().valueOf()}`
     this.checkAvailabilityButton = '#passHolderReservationsSearchButton'
+    this.selectResortDropdown = '#PassHolderReservationComponent_Resort_Selection'
   }
 
   async navigate() {
     await this.page.goto(this.url)
     await this.page.waitForSelector(this.checkAvailabilityButton)
+    await this.page.select(this.selectResortDropdown, '6')
+    await this.page.click(this.checkAvailabilityButton)
   }
 
   async listAvailability() {
-    let response = await this.page.evaluate(() => fetch(this.availabilityUrl, {
+    return await this.page.evaluate(url => fetch(url, {
       mode: 'cors',
       credentials: 'include',
       referrer: 'https://www.epicpass.com/plan-your-trip/lift-access/reservations.aspx?reservation=true',
       referrerPolicy: 'strict-origin-when-cross-origin'
-    }).then(res => res.json()))
-    console.log(response)
-    return response
+    }).then(res => res.json()), this.availabilityUrl())
   }
 }
-
-fetch(`https://www.epicpass.com/api/LiftAccessApi/GetCapacityControlReservationInventory?resortCode=9&startDate=12%2F1%2F2020&endDate=12%2F31%2F2020&_=${moment().valueOf()}`, {
-  "headers": {
-    "accept": "application/json, text/javascript, */*; q=0.01",
-    "accept-language": "en-US,en;q=0.9",
-    "sec-ch-ua": "\";Not\\\\A\\\"Brand\";v=\"99\", \"Chromium\";v=\"88\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "x-queueit-ajaxpageurl": "https%3A%2F%2Fwww.epicpass.com%2Fplan-your-trip%2Flift-access%2Freservations.aspx%3Freservation%3Dtrue",
-    "x-requested-with": "XMLHttpRequest"
-  },
-  "referrer": "https://www.epicpass.com/plan-your-trip/lift-access/reservations.aspx?reservation=true",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "include"
-}).then(res => res.text()).then(console.log)
-
-fetch(`https://www.epicpass.com/api/LiftAccessApi/GetCapacityControlReservationInventory?resortCode=9&startDate=12%2F1%2F2020&endDate=12%2F31%2F2020&_=${moment().valueOf()}`, {
-  "referrer": "https://www.epicpass.com/plan-your-trip/lift-access/reservations.aspx?reservation=true",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "include"
-}).then(res => res.text()).then(console.log)
